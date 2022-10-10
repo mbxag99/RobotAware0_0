68c4fd2a67eedfca3d8316f42005d0e81e04b871
Agbaria Weaam <weaam.ag.003@gmail.com>
2022-10-10 13:32:19 +0300

    some explanation

--- VisualOdometry/videoProcessing/main.py
+++ VisualOdometry/videoProcessing/main.py
@@ -15,12 +15,17 @@ def video_feed():
 def get_frames():
     ourVideo = cv2.VideoCapture('ourVideo.mp4')
     while ourVideo.isOpened():
+        #success is a bool data type, returns true if Python is able to read the videoCapture() object.
+        #frame is a numpy array, represents the first image that video captures.
+        #read() returns a bool (True/False). If frame is read correctly, it will be true
         success, frame = ourVideo.read() 
         if not success:
             break
         else:
             height = ourVideo.get(cv2.CAP_PROP_FRAME_HEIGHT)
             width = ourVideo.get(cv2.CAP_PROP_FRAME_WIDTH)
+            #cv2.imencode() function is to convert(encode) the image format into streaming data and assign
+            # it to memory cache.
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
