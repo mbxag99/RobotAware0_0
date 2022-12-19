63ab73ae85dbce8dd8c79972e07377d8d2f91c44
mbxag99 <mbx.ag99@gmail.com>
2022-12-19 21:41:53 +0200

    Stop Missin around

--- VisualOdometry/AutoCar/main_ac.py
+++ VisualOdometry/AutoCar/main_ac.py
@@ -8,8 +8,9 @@ import cv2
 import numpy as np
 import imutils
 from ProcessFrame import Processing
-
-
+from time import time, sleep
+import math
+from  ProcessFrame_w import HandCodedLaneFollower
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
@@ -18,19 +19,64 @@ CORS(app)
 socketio.init_app(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 counter = 0
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app,host='10.0.0.16',port='5000', debug=True,use_reloader=True)
 
+@socketio.on('connect')
+def on_connect():
+    print('A client connected!')
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('A client disconnected!')
+    
 @socketio.on('video_slice')
 def handle_frame(data):
-    global counter 
+    #global counter 
     print("Received video slice data")# we received a video 
     socketio.emit('pause',broadcast=True)
-    path = 'C:/Users/windows 10/Desktop/superP/VisualOdometry/AutoCar/v' + str(counter) + '.webm'
+    path = 'C:/Users/windows 10/Desktop/superP/VisualOdometry/AutoTest/inc_vid_slices/v' + str(counter) + '.webm'
     with open(path, 'wb') as f:
         f.write(data)
     counter+=1
-    Processing.start_processing()
-    socketio.emit('resume',broadcast=True)
+    direc , duration = handleVideo(path)
+    # if direc is forwared we can cotinue receiving video slices immediately
+    # if direc is left then we need the recording to be halted as we need to send a command to the phone to go left then after turning then we can continue receiving video slices
+    # if direc is right then we need the recording to be halted as we need to send a command to the phone to go right then after turning then we can continue receiving video slices
+    socketio.emit('command_to_phone',(direc,duration))
+    if direc == 'F':
+        socketio.emit('resume',broadcast=True)
+    else:
+        # time out for 2 seconds the emit resume
+        sleep(2)
+        socketio.emit('resume',broadcast=True)
+
+
+@socketio.on('test')
+def handle():
+    print('COMMMMMAND')
+    aa_milne_arr = ['F', 'B', 'R', 'L']
+    ran_comm = np.random.choice(aa_milne_arr, 1, p=[0.5, 0.1, 0.1, 0.3])
+    print(ran_comm[0])
+    sleep(5)
+    socketio.emit('command_to_phone',ran_comm[0])
+
+
+def handleVideo(path):
+        calso = HandCodedLaneFollower()
+        calso.test_video(path)
+        print("Done!") 
+        # most frequent direction keep in mind that the direction is a tuple (direction , angle)
+        last_half = calso.dirc[len(calso.dirc)//2:]
+        most_freq_dir = max(calso.dirc, key=calso.dirc.count)
+        average_angle = sum([x[1] for x in calso.dirc if x[0] == most_freq_dir[0]]) / len([x[1] for x in calso.dirc if x[0] == most_freq_dir[0]])
+        print('most frequent direction is ', most_freq_dir[0], 'with average angle of ', average_angle)
+        turning_radius = 10 # cm
+        circumference = 2 * math.pi * turning_radius
+        distance = circumference * (average_angle / 360)
+        turning_speed = 10 # cm/s
+        duration = distance / turning_speed
+        return most_freq_dir , duration
+
 
     
 
--- phoneapp/MonocularTemp.js
+++ phoneapp/MonocularTemp.js
@@ -13,40 +13,53 @@ import {useDispatch, useSelector} from 'react-redux';
 import {action} from './actionfigure';
 import RNBluetoothClassic from 'react-native-bluetooth-classic';
 import {io} from 'socket.io-client';
-let socket_to_ac = io(`http://10.0.0.16:5000`, {
-  transports: ['websocket'],
+let socket_to_ac = io(`http://10.0.0.16:5000/`, {
+  forceNew: true,
+});
+
+socket_to_ac.on('error', e => {
+  console.log('errere ac ' + e);
+});
+socket_to_ac.on('connect-error', e => {
+  console.log('errere ac ' + e);
 });
 let actiony = new action();
 export default function MonocularTemp() {
   const [status, setStatus] = useState(0);
   const dispatch = useDispatch();
-
+  const [isBluetoothEnabled, setIsBluetoothEnabled] = useState(false);
   useEffect(() => {
     myFUN(false);
     //console.log('pitof');
   }, []);
 
-  const funcc = () => {
-    actiony.set_listeners_for_ac(RNBluetoothClassic);
-  };
-
-  const another_func = async () => {
+  const setup_listeners = async () => {
     try {
-      //console.log('dassadasdw222 ', socket.connected);
       console.log('Sytttt ', socket_to_ac.connected);
-      //console.log(await RNBluetoothClassic.isBluetoothEnabled());
-      //console.log(await RNBluetoothClassic.getBondedDevices());
-      //console.log(await RNBluetoothClassic.getConnectedDevices());
-      await RNBluetoothClassic.connectToDevice('98:D3:31:20:68:84');
+      socket_to_ac.emit('test');
       socket_to_ac.on('command_to_phone', async command => {
         console.log('commnad ' + command);
         await RNBluetoothClassic.writeToDevice('98:D3:31:20:68:84', command);
-        //socket_to_ac.emit('phone_sent_to_robot');
-        socket_to_ac.emit('phone');
       });
-      socket_to_ac.emit('phone');
+      socket_to_ac.on('pause', async () => {
+        console.log('pause');
+        await RNBluetoothClassic.writeToDevice('98:D3:31:20:68:84', 's');
+      });
     } catch (error) {
       console.log('errrrrr ', error);
+    }
+  };
+
+  const bluetooth_connect = async () => {
+    try {
+      await RNBluetoothClassic.connectToDevice('98:D3:31:20:68:84');
+      console.log('connected');
+      setIsBluetoothEnabled(true);
+    } catch (error) {
+      // if connection failed, you will get an error and in the app the user will be notified
+      console.log('errrrrr ', error);
+      // notify the user
+      alert('Bluetooth Connection failed');
     }
   };
   const myFUN = stopped => {
@@ -74,7 +87,7 @@ export default function MonocularTemp() {
           })
           .then(stream => {
             actiony.start_streaming(stream, setStatus, stopped);
-            another_func();
+            setup_listeners();
           })
           .catch(error => {
             console.log('erereerddd ' + error);
@@ -109,12 +122,16 @@ export default function MonocularTemp() {
           <Text> Click Me </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.goBack}>
+      <View
+        style={[
+          styles.cornerButton,
+          {backgroundColor: isBluetoothEnabled ? '#007AFF' : 'red'},
+        ]}>
         <TouchableOpacity
           onPress={() => {
-            //setPage('Home');
+            bluetooth_connect();
           }}>
-          <Text>POT</Text>
+          <Text>Connect To Blue</Text>
         </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
@@ -137,9 +154,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   connection: {
-    height: 50,
-    width: 50,
+    height: 80,
+    width: 80,
     borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   rtc: {
     height: 200,
@@ -156,5 +175,14 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     margin: 10,
+  },
+  cornerButton: {
+    position: 'absolute',
+    top: 350,
+    right: 20,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
